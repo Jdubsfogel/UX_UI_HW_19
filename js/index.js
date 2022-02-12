fa6add3a4b183c7a20327fdd5029f51f12afded2
Jdubsfogel <97627540+Jdubsfogel@users.noreply.github.com>
2022-02-12 07:48:03 -0600

    Initial Project Changes
    Feb 12
    
    8am

---
+++ js/index.js
@@ -0,0 +1,29 @@
+console.log("Your index.js file is loaded correctly!")
+;
+
+
+
+$(document).ready(function(){
+    $("div#hotdog1").hover(function(){
+      $("div#hotdog1").toggleClass("newcardBackground");
+    });
+});
+
+$(document).ready(function(){
+    $("div#hotdog2").hover(function(){
+      $("div#hotdog2").toggleClass("newcardBackground");
+    });
+});
+
+$(document).ready(function(){
+    $("div#hotdog3").hover(function(){
+      $("div#hotdog3").toggleClass("newcardBackground");
+    });
+});    
+
+$(document).ready(function(){
+    $("div#hotdog4").hover(function(){
+      $("div#hotdog4").toggleClass("newcardBackground");
+    });
+});
+
